6c8fd8a570e263c6aaeb7e5ba2becc11332df74e
Malcome Baggett <MalcomeBaggett@gmail.com>
2024-02-16 15:50:37 -0500

    finsished first post req

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the API"}
+
 
 @app.get("/posts")
 def get_posts():
@@ -12,3 +16,9 @@ def get_posts():
         {"id": 1, "title": "First Post", "content": "This is the first post"},
         {"id": 2, "title": "Second Post", "content": "This is the second post"},
     ]
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
