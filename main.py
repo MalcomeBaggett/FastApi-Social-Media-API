00b213a6a20b7144d25c58eae225c354dadd264e
Malcome Baggett <MalcomeBaggett@gmail.com>
2024-02-19 18:50:00 -0500

    added get post by id endpont

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "First Post", "content": "This is the first post", "id": 1},
+    {"title": "Second Post", "content": "This is the second post", "id": 2},
+]
+
+
+def find_post(post_id):
+    for post in my_posts:
+        if post["id"] == post_id:
+            return post
+    return None
 
 
 @app.get("/")
@@ -12,13 +34,25 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return [
-        {"id": 1, "title": "First Post", "content": "This is the first post"},
-        {"id": 2, "title": "Second Post", "content": "This is the second post"},
-    ]
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
+@app.post("/posts")
+def create_post(new_post: Post):
+
+    # if you ever need to convert a pydantic model to a dictionary, you can use the .dict() method
+    post_dict = new_post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": f"{post_dict} is created successfully"}
+
+
+# title str, content str
+
+
+@app.get("/posts/{post_id}")
+def get_posts(post_id: int):
+    post = find_post(post_id)
+    if post:
+        return {"data": post}
+    return {"data": "Post not found"}
